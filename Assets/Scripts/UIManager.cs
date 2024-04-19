1a4a4ffd46d0f4eaa672befabc15920b5f600eb6
mfj4000 <mjager20@student.aau.dk>
2024-04-19 10:55:11 +0200

    more ui stuffs

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,42 +4,20 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
-using UnityEngine.Video;
 
 public class UIManager : MonoBehaviour
 {
-    public RawImage infoVideoPlayer;
-    public VideoPlayer[] infoVideos;
+    public GameObject[] infoAnimations;
     public TextMeshProUGUI[] infoTexts;
     public Image[] dotMenus;
-    private RenderTexture renderTexture;
 
-    private int currentVideoIndex = 0;
+    private int currentAnimationIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Create a new RenderTexture with the same dimensions as the RawImage
-        renderTexture = new RenderTexture((int)infoVideoPlayer.rectTransform.rect.width,
-                                           (int)infoVideoPlayer.rectTransform.rect.height, 0);
-        renderTexture.Create();
-
-        // Set the RenderTexture as the target texture for all VideoPlayers
-        foreach (VideoPlayer videoPlayer in infoVideos)
-        {
-            videoPlayer.targetTexture = renderTexture;
-            videoPlayer.Stop(); // Stop all videos initially
-        }
-
-        // Set the initial video player to play
-        PlayVideo(0);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // Update the RawImage texture to the RenderTexture
-        infoVideoPlayer.texture = renderTexture;
+        // Set the initial animation to play
+        PlayAnimation(0);
     }
 
     public void QRButtonClick()
@@ -49,16 +27,15 @@ public class UIManager : MonoBehaviour
 
     public void GlobalInfoClick()
     {
-        int nextVideoIndex = (currentVideoIndex + 1) % infoVideos.Length;
-        PlayVideo(nextVideoIndex);
+        int nextAnimationIndex = (currentAnimationIndex + 1) % infoAnimations.Length;
+        PlayAnimation(nextAnimationIndex);
     }
 
-    void PlayVideo(int index)
+    void PlayAnimation(int index)
     {
-        foreach (VideoPlayer videoPlayer in infoVideos)
+        foreach (GameObject animationObject in infoAnimations)
         {
-            videoPlayer.Stop();
-            videoPlayer.gameObject.SetActive(false);
+            animationObject.SetActive(false);
         }
 
         foreach (TextMeshProUGUI text in infoTexts)
@@ -66,17 +43,17 @@ public class UIManager : MonoBehaviour
             text.gameObject.SetActive(false);
         }
 
-        foreach (Image image in dotMenus){
+        foreach (Image image in dotMenus)
+        {
             image.gameObject.SetActive(false);
         }
 
-        infoVideos[index].gameObject.SetActive(true);
-        infoVideos[index].Play();
+        infoAnimations[index].SetActive(true);
 
         infoTexts[index].gameObject.SetActive(true);
 
         dotMenus[index].gameObject.SetActive(true);
 
-        currentVideoIndex = index;
+        currentAnimationIndex = index;
     }
 }
